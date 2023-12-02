ac00c5ac123515eeda33b41399b520d922cd1ac4
binary-h0 <max000807@gmail.com>
2023-12-02 17:50:46 +0900

    Feat: #9 권한 확인 및 옵션 값 검증 구현 완료

--- include/heat.h
+++ include/heat.h
@@ -1,6 +1,7 @@
 #ifndef HEAT_H
 #define HEAT_H
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,5 +11,18 @@
 
 #include "option_handler.h"
 #include "signal_mapper.h"
+
+extern int errno;
+
+void process_info_print();
+int verify_options(opts *options);
+void verify_command(char *command);
+void verify_script(char *script);
+void verify_recovery(char *script);
+void verify_fail(char *script);
+void check_file_permission(char *filename);
+
+void update_environment(opts *options);
+int build_sentinel_process(opts *options);
 
 #endif
